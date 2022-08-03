07075f0d8ac9efc2cacd366f0791a32413e76910
kirillram <kirill.bairamov@gmail.com>
2022-08-03 15:01:42 +0800

    Initial Commit

---
+++ CreditMix/CreditMixApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreditMixApp.swift
+//  CreditMix
+//
+//  Created by Кирилл on 03.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct CreditMixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
